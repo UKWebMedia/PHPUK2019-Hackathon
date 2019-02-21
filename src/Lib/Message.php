fc2154cf1eeeee57c7e21b8937185ed7933141d3
Daniel Platt <bitbucket@ofdan.co.uk>
2019-02-21 13:12:19 +0000

    message class contents

--- src/Lib/Message.php
+++ src/Lib/Message.php
@@ -2,10 +2,33 @@
 
 namespace App\Lib;
 
+use Nexmo\Client;
+
 class Message
 {
+    /** @var Client */
+    private $client;
+
+    function __construct()
+    {
+        $this->generateClient();
+    }
+
     public function reminder(string $phoneNumber, string $talkName, string $track, string $floor, \DateTime $startTime)
     {
+        $difference = $startTime->diff(new \DateTime());
+        $minutes = $difference->format('i');
 
+        $message = $this->client->message()->send([
+            'to' => $phoneNumber,
+            'from' => 'Reminder Service',
+            'text' => 'Reminder: ' .$talkName . ' is starting in ' . $minutes . '. The room is ' . $track . ' which can be found on ' . $floor,
+        ]);
+    }
+
+    private function generateClient()
+    {
+        $creds = new \Nexmo\Client\Credentials\Basic(env('NEXMO_API_KEY'), env('NEXMO_API_SECRET'));
+        $this->client = new Client($creds);
     }
 }
